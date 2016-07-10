bd8c8c90b077e18a32daf8b38f16ca2e3ad57b9d
RubaXa <ibnRubaXa@gmail.com>
2016-07-10 21:04:36 +0300

    * upd

--- src/ui/_entry-point.ts
+++ src/ui/_entry-point.ts
@@ -2,9 +2,11 @@ import {rfunction} from 'rdot'
 import authFrom from './auth-form';
 
 export default rfunction<string>(():string => {
-	return `
-		<div class="logged-out env-production macintosh session-authentication page-responsive min-width-0">
-			${authFrom()}
-		</div>
-	`
+	const session = sessionStream.get();
+
+	if (session.auth) {
+
+	} else {
+		return authFrom();
+	}
 })
